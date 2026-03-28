45b034366df22e7f28541f3fbd3b038d7a7b98e7
SaraRadulovic003 <radulovics003@gmail.com>
2026-03-28 12:43:38 +0100

    Kreirana klasa Klasa koja implementira interfejs specifikacijainterfejsa

---
+++ src/magacin/Klasa.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import magacin.interfejs.specifikacijainterfejsa;
+
+public class Klasa implements specifikacijainterfejsa {
+
+	@Override
+	public void dodajArtikal(Artikl artikl) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikl artikl) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikl pronadjiArtikl(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
